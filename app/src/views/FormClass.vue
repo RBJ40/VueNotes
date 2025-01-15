57c94abe72a5e738822483d3ed1155dc7404d05e
RBJ40 <celinechan268@gmail.com>
2025-01-15 12:30:39 -0500

    draft

---
+++ app/src/views/FormClass.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <form>
+      <input type="text" value="Username" />
+      <input type="text" value="Password" />
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>

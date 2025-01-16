b6c0c773406e1b1f7ba1984999afe84f89042c1d
RBJ40 <celinechan268@gmail.com>
2025-01-16 12:25:37 -0500

    Update FormClass.vue

--- app/src/views/FormClass.vue
+++ app/src/views/FormClass.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <form>
-      <input type="text" value="Username" />
-      <input type="text" value="Password" />
-      <input type="submit" value="Submit" />
+    <form name="login-form" @submit.prevent="login(user)">
+      <label for="username">Enter username</label>
+      <input type="text" v-model="user.username" />
+      <label for="password">Enter password</label>
+      <input type="password" v-model="user.password" />
+      <button @click="increment">Click me</button>
+      <button class="submit-button" type="submit">Login</button>
     </form>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+const user = reactive({ username: '', password: '' })
+function login(user) {
+  console.log(user)
+}
+</script>
 
 <style scoped></style>

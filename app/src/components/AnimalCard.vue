3e5813ec79ebb216b6f6c3442dd197b10b97499c
RBJ40 <celinechan268@gmail.com>
2025-01-30 12:12:01 -0500

    draft

---
+++ app/src/components/AnimalCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <AnimalCard v-for="animal in animals" :key="animal.name" />
+  </div>
+</template>
+
+<script setup>
+const animals = [{}]
+</script>
+
+<style lang="scss" scoped></style>
--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <NewList />
+    <h2>{{ animal.name }}</h2>
+    <img :src="animal.image" />
   </div>
 </template>
 
 <script setup>
+import AnimalCard from '..components/AnimalCard.vue'
 import NewList from '../components/NewList.vue'
+defineProps({
+  animal: Object,
+})
 </script>
 
 <style scoped></style>
